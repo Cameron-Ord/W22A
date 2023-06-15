2c081ce801ffdb8c784c10dd97ce61644efe9e32
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-14 22:27:01 -0600

    get request functional

--- python/app.py
+++ python/app.py
@@ -1,19 +1,20 @@
 #importing
 import json
 from flask import Flask,request, make_response, jsonify
-import dbhelper, api_helper
+import dbhelper, api_helper, dbcreds
+import flask_cors
 app = Flask(__name__)
 
-@app.post('/api/candy')
+@app.get('/api/candy')
 #function gets called on api request
 def return_all():
    try:
       #calls the function in api_helper to loop through the information sent
-         error=api_helper.check_endpoint_info(request.json, ['']) 
+         error=api_helper.check_endpoint_info(request.args, []) 
          if(error !=None):
             return make_response(jsonify(error), 400)
          #calls the procedure to insert sent information into the DB
-         results = dbhelper.run_proceedure('CALL return_all(?)', [request.json.get('')])
+         results = dbhelper.run_proceedure('CALL return_all()', [])
          #returns results from db run_procedure
          if(type(results) == list):
             return make_response(jsonify(results), 200)
@@ -27,4 +28,15 @@ def return_all():
       print('something went wrong')
 
 #running @app
-app.run(debug=True)
+if(dbcreds.production_mode == True):
+   print()
+   print('----Running in Production Mode----')
+   print()
+   app.run(debug=True)
+else:
+   from flask_cors import CORS
+   CORS(app)
+   print()
+   print('----Running in Testing Mode----')
+   print()
+   app.run(debug=True)
